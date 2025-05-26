1f974237a77e9a49ad67ac4bc8a301f4da6d3786
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2025-05-26 23:10:21 +0900

    feat : setting files

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        background: resolve(__dirname, 'src/background.ts'),
+        contents:   resolve(__dirname, 'src/contents.ts'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: 'chunks/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  }
+});
